dc444975f1db56ed87d7d9c6d7286e5a257013d9
Carlos Vechi <161260318+carlosvechi@users.noreply.github.com>
2026-02-12 20:52:32 -0300

    Exibe navbar e footer após login validado

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
-  <LoginView />
+  <LoginView v-if="!isAuthenticated" @login-success="isAuthenticated = true" />
+  <template v-else>
+    <TheNavBar />
+    <TheTemplate />
+    <TheFooter />
+  </template>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import TheFooter from './components/TheFooter.vue'
+import TheNavBar from './components/TheNavBar.vue'
 import LoginView from './views/LoginView.vue'
+import TheTemplate from './views/TheTemplate.vue'
+
+const isAuthenticated = ref(false)
 </script>

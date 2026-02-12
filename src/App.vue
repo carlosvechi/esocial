b9878b6eddf7b106de66190d6b6a2e69238ca5ba
carlosvechi <juniorvechi@hotmail.com>
2026-02-12 20:56:38 -0300

    footer e navbar arrumados

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
   <LoginView v-if="!isAuthenticated" @login-success="isAuthenticated = true" />
-  <TheTemplate v-else />
+
+  <template v-else>
+    <TheNavBar />
+    <TheTemplate />
+    <TheFooter />
+  </template>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+
+import TheFooter from './components/TheFooter.vue'
+import TheNavBar from './components/TheNavBar.vue'
+
 import LoginView from './views/LoginView.vue'
 import TheTemplate from './views/TheTemplate.vue'
 

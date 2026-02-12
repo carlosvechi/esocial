32630722e6830f25f8d4a0b31e6b766b494aa0ee
Carlos Vechi <161260318+carlosvechi@users.noreply.github.com>
2026-02-12 20:47:19 -0300

    Redireciona para template após validação de login

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
-  <LoginView />
+  <LoginView v-if="!isAuthenticated" @login-success="isAuthenticated = true" />
+  <TheTemplate v-else />
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import LoginView from './views/LoginView.vue'
+import TheTemplate from './views/TheTemplate.vue'
+
+const isAuthenticated = ref(false)
 </script>

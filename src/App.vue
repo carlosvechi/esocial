651ceaac2cdc2900a521817a534e8641103e6a3b
Carlos Vechi <161260318+carlosvechi@users.noreply.github.com>
2026-02-12 20:28:29 -0300

    refactor: isolate main screen layout to reduce merge conflicts

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <template>
-  <template v-if="isAuthenticated">
-    <TheNavBar />
-    <TheTemplate />
-    <TheFooter />
-  </template>
-
+  <MainView v-if="isAuthenticated" />
   <LoginView v-else @login-success="handleLoginSuccess" />
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import TheFooter from './components/TheFooter.vue'
-import TheNavBar from './components/TheNavBar.vue'
 import LoginView from './views/LoginView.vue'
-import TheTemplate from './views/TheTemplate.vue'
+import MainView from './views/MainView.vue'
 
 const isAuthenticated = ref(false)
 

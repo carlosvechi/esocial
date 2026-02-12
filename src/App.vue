a35893797082571908beddbb4697f9835a952827
Carlos Vechi <161260318+carlosvechi@users.noreply.github.com>
2026-02-12 20:41:59 -0300

    fix: align App.vue with LoginView entry to avoid merge conflict

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,7 @@
 <template>
-  <TheNavBar />
-  <TheElder />
-  <TheFooter />
+  <LoginView />
 </template>
 
-<script>
-import TheFooter from './components/TheFooter.vue';
-import TheNavBar from './components/TheNavBar.vue';
-import TheElder from './views/TheTemplate.vue';
-
-
-export default {
-  name: 'App',
-  components: {
-   TheElder,
-   TheNavBar,
-   TheFooter
-  }
-}
+<script setup>
+import LoginView from './views/LoginView.vue'
 </script>
-
-<style>
-body {
-  background-color: #ffffff !important;
-}
-html {
-  scroll-behavior: smooth;
-  scroll-padding-top: 90px; /* altura da navbar */
-}
-
-
-</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<template>
+  <TheNavBar />
+  <TheTemplate />
+  <TheFooter />
+</template>
+
+<script setup>
+import TheFooter from '../components/TheFooter.vue'
+import TheNavBar from '../components/TheNavBar.vue'
+import TheTemplate from './TheTemplate.vue'
+</script>
